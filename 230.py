4cf36a7932d8850b28f98b9a9aa7fa706a5abd32
Vaibhav Singh <vishucodplayer@gmail.com>
2025-08-13 16:06:40 +0530

    Create 230.py

---
+++ 230.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        counter=[0]
+        res=[0]
+
+        def dfs(root):
+
+            if root is None:
+                return 
+            dfs(root.left)
+            counter[0]+=1
+            if counter[0]==k:
+                res[0]=root.val
+    
+               
+                
+            dfs(root.right)
+    
+        dfs(root)
+
+        return res[0]
+
+
+        
